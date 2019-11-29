90f1b57f51e5f041b627125102007da126115422
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-11-29 15:59:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -133,7 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     dennis_ritchies_new_info = programmer_hash[:dennis_ritchie][:languages] = ["Smalltalk", "LISP", "Assembly"]
-     return programmer_hash
+    
 
 end
