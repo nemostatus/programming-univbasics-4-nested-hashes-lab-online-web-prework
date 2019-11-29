2569eea91e6a29d2567d791db7a853f89be99b86
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-11-29 15:38:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,9 +70,7 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash= {:yukihiro_matsumoto}
-	programmer_hash[:known_for] = "ruby"
-	programmer_hash[:languages] = ["LISP","C"]
+	
 {
         :grace_hopper => {
           :known_for => "COBOL",
